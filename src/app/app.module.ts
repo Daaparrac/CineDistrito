636abdb152908b60f068c3714a738906727f5fa5
Daniel parra <danielparrac26@gmail.com>
2020-07-21 12:14:35 -0500

    pagina de inicio

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ComponentesModule } from './componentes/componentes.module';
+import { HomeComponent } from './pages/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ComponentesModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  peliculas = [
+    {
+      "img" : "assets/pe1.jpg"
+    },
+    {
+      "img" : "assets/pe2.jpg"
+    },
+    {
+      "img" : "assets/pe3.jpg"
+    },
+    {
+      "img" : "assets/pe4.jpg"
+    },
+    {
+      "img" : "assets/pe5.jpg"
+    },
+    {
+      "img" : "assets/pe6.jpg"
+    },
+    {
+      "img" : "assets/pe7.jpg"
+    }
+  ];
+
+  peliculas2 = [
+    {
+      "img" : "assets/pe8.jpg"
+    },
+    {
+      "img" : "assets/pe9.jpg"
+    },
+    {
+      "img" : "assets/pe10.jpg"
+    },
+    {
+      "img" : "assets/pe11.jpg"
+    }
+  ]
+}
